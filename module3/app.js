d2269a9c3e6760933481323fb66eefaef1a56021
AlexeYarko <alexeyarkoedu@gmail.com>
2017-01-31 13:46:18 +0200

    Assignment 1.Module 3. v# 1. fixed 2 app.js

--- module3/app.js
+++ module3/app.js
@@ -3,12 +3,8 @@
 
 angular.module('MenuCategoriesApp', [])
 .controller('MenuCategoriesController', MenuCategoriesController)
-//.controller('NarrowitDownController', NarrowitDownController)
 .service('MenuCategoriesService', MenuCategoriesService)
-//.directive('foundItems', FoundItemsDirective)
-//.constant('ApiBasePath', "http://cours.com:8080/lecture25")
 .directive('shoppingList', ShoppingListDirective)
-
 .constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
 function ShoppingListDirective() {
@@ -16,7 +12,7 @@ function ShoppingListDirective() {
     templateUrl: 'shoppingList.html',
     
 	scope: {
-    items2: '=', //lecture 29
+    items2: '=', 
     onRemove: '&'
     }, 
     controller: ShoppingListDirectiveController,
@@ -30,7 +26,7 @@ function ShoppingListDirective() {
 function ShoppingListDirectiveController() {
   var list = this;
 
-  list.cookiesInList = function () {     // ЕСЛИ ЭЛЕМЕНТЫ НЕ НАЙДЕНЫ!!!!!
+  list.cookiesInList = function () {    
 	if (list.items2.length == 0) {
 //	console.log("cookiesInList - true");
 	return true;
@@ -42,18 +38,8 @@ function ShoppingListDirectiveController() {
 	}
   };
  //////
-  list.cookiesInList2 = function () {     // ЕСЛИ ЭЛЕМЕНТЫ НЕ НАЙДЕНЫ!!!!!
-	 //console.log('cookiesInList');
-/*      for (var i = 0; i < list.items.length; i++) {
-      var name = list.items[i].name;
-      if (name.toLowerCase().indexOf("cookie") !== -1) {
-        return true;
-      }
-    }  */
-/*     if (items.foundItems.length == 0) {
-	return true;
-	} */
-//	console.log('cookiesInList2');
+  list.cookiesInList2 = function () {     
+
     return true;
   }; 
   
@@ -61,39 +47,7 @@ function ShoppingListDirectiveController() {
 
 
 
-/* function NarrowitDownController(MenuSearchService) {
-  var menu = this; 
-  
-  menu.searchMenuItems = function () {
-  menu.items = [];
-  if(menu.searchTerm){
-    console.log('menu.searchTerm  '+menu.searchTerm)
-    var promise = MenuSearchService.getMatchedMenuItems(menu.searchTerm);
-    promise.then(function (response) {
-      
-      console.log('response '+response);
-      if(response.length == 0) {
-            menu.items = [];
-            menu.errorMessage = "Nothing found";
-			console.log('menu.errorMessage '+menu.errorMessage);
-          } else {
-            menu.items = response;
-            menu.errorMessage = "";
-          }
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-  }
-  else{
-    menu.errorMessage = "Nothing found";
-  }
-};
 
- menu.removeItem = function (itemIndex) {
-    menu.items.splice(itemIndex,1);
-  }
-}; */
 
  MenuCategoriesController.$inject = ['MenuCategoriesService'];
 function MenuCategoriesController(MenuCategoriesService) {
@@ -107,111 +61,94 @@ menu.errorMessage = "";
  return countFoundItems;
  }
  
-menu.searchMenuItems = function () {   //КНОПКА НАЖАТА!!!!	 
+menu.searchMenuItems = function () {   
  
-if(menu.searchTerm){                   // ЕСЛИ ПОЛЕ НЕ ПУСТОЕ
+if(menu.searchTerm){                 
   menu.foundItems = [];
   
   var i = false;
-console.log("Кнопка нажата и в Поле ввода есть текст!");
+
   var promise = MenuCategoriesService.getMenuForCategory(); // delete getMenuCategories();
    
    promise
    
-    /* .then(function () {
-        return MenuCategoriesService.ClearArray();
-      })   */ 
+
   
-  
-    .then(function (response) {   // ДОБАВИТЬ ПРОВЕРКУ ПУСТОГО МАССИВА!!------------------------------------
+    .then(function (response) {   
     
 	menu.menu_it = response.data.menu_items;        // !!!!!        
 	
-	//console.log("menu.menu_it "+menu.menu_it);   ///  G O O D ! !
-	//console.log("menu.menu_it.length "+menu.menu_it.length);  // 219
+
 	 MenuCategoriesService.ClearArray();
                 
-	console.log("получено записей "+menu.menu_it.length);
+
 	 
 		for (var index = 0; index < menu.menu_it.length; index++) { 
           var m = menu.menu_it[index].short_name;
-		  //var i = false;
+
 		  var vv = function (m) {
            return m.indexOf(menu.searchTerm) !== -1;
            }
 		  
 		  if (vv(m)) {
-            //console.log("Кнопка нажата и Поле ввода заполнено! Нашли записи!!!!!!");
+ 
 			i = true;
-			//замена
+
             menu.foundItems.push(menu.menu_it[index]);
-			//menu.AddFoundItem(index);
+
 			MenuCategoriesService.AddFoundItem(menu.menu_it[index]);
-			//console.log("index "+index); // insert new items !!!
-			//console.log(menu.getItems());
-			//console.log("m "+m);
-			//console.log("vv "+vv(m));
+
           }
         } 
 
         if (i == true) {
 		    menu.errorMessage = "";
-            console.log("i = true. Поле заполнено и Кнопка нажата! Нашли записи!!!!!!");
+        
         }
         else
         {
 			menu.errorMessage = "Nothing found";
-            console.log("i = false. Поле заполнено и Кнопка нажата! Nothing found!!!!!!!");
+          
         } 		
-	//console.log("menu.searchTerm "+menu.searchTerm);
-//	console.log("foundItems -------------------"+menu.foundItems.length); //МАССИВ ИЗ РЕЗУЛЬТАТАМИ ФИЛЬТРА
-//	console.log("foundItems2 (не очищается!!)--"+MenuCategoriesService.getColFoundItems());
+
 	
 	
 	
   })
   .catch(function (error) {
-    console.log("Something went terribly wrong.");     //   Nothing found!
+ 
   }); 
 }
-else{                                      // ЕСЛИ ПОЛЕ ПУСТОЕ
+else{                                     
     menu.errorMessage = "Nothing found";
-	//если нажата кнопка, то - Nothing found!
-	console.log("Поле не заполнено и Кнопка нажата! Nothing found!");  
+	
+	
   }
   }
   
-   menu.addItem = function () {
-     //menu.foundItems = [];   
-   //shoppingList.addItem(list.itemName, list.itemQuantity);
-   console.log("add --");
-  }
-   // ! ! !  О Ш И Б К А ! ! ! ! ! !
+
+
    menu.removeItem = function (itemIndex) {
-    console.log("delete 1 --"+itemIndex);
-    //menu.foundItems.splice(itemIndex,1);
+
+
 	MenuCategoriesService.DelItem(itemIndex);
 	
 	
   }
   
     menu.remLog = function (itemIndex) {
-    console.log("delete 2 --"+itemIndex);	
-	//menu.foundItems.splice(itemIndex,1);
+	
+
   }
-  //замена foundItems
- // menu.getItems = MenuCategoriesService.getFoundItems();
-  //showList.boughtList = ShoppingListCheckOffService.getItemsList2();
+
 
   menu.getItems = function () {
     return MenuCategoriesService.getFoundItems();
 	
-	//console.log("delete 2 --"+itemIndex);	
-	//menu.foundItems.splice(itemIndex,1);
+
   }
   } 
 
-// OK!!!
 MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
 
 function MenuCategoriesService($http, ApiBasePath) {
@@ -219,14 +156,13 @@ function MenuCategoriesService($http, ApiBasePath) {
   var items = [];
   var foundItems2 = [];
 
-   service.getMenuForCategory = function (searchTerm) {     //del par shortName
+   service.getMenuForCategory = function (searchTerm) {     
     var response = $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json"),
     });
 	
-	 //console.log("items --	"+items); 
-     //console.log("response --	"+response); 
+
     return response;
   }; 
   
@@ -241,18 +177,18 @@ function MenuCategoriesService($http, ApiBasePath) {
     service.AddFoundItem = function (ind) {
     var i = ind;
 	foundItems2.push(i);
-	//to_buy.splice(ind, 1);
+
   };
   
     service.DelItem = function (ind) {
     var i = ind;
 	foundItems2.splice(ind, 1);
-	//to_buy.splice(ind, 1);
+
   };
     service.ClearArray = function () {
-    //var i = ind;
+
 	foundItems2 = [];
-//	console.log("Array foundItems2 is empty!!!"); 
+
   };
  
 }
